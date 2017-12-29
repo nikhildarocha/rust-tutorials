c0db4a9e9d29bbcd7c4808a215bd66e261107220
Nikhil Da Rocha <niks77883@gmail.com>
2017-12-29 23:54:37 +0530

    rust tutorials intro

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,25 @@
+
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+
+
+fn main() {
+    println!("Hello, world!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("Guess the number");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+
+
+    println!("You guessed: {}", guess);
+
+}
