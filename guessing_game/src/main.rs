6b3b3a6a3eafb9f695d4cc8528804f89d37ab260
Nikhil Da Rocha <niks77883@gmail.com>
2017-12-29 23:59:39 +0530

    rust tutorials random library

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,10 +6,14 @@ use rand::Rng;
 
 
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+
+    println!("Guess the number");
+
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("Guess the number");
+
+    println!("The secret number is: {}", secret_number );
 
     println!("Please input your guess.");
 
